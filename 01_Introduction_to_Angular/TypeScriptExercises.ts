317a2ee8ddd32e377de76c263c5e918df6df3e0c
Kostadin Belchev <kostadin.belchev@gmail.com>
2018-07-22 18:50:09 +0300

    starting exercises part 1

--- 01_Introduction_to_Angular/TypeScriptExercises.ts
+++
@@ -1,6 +0,0 @@
-class Request {
-
-  constructor(method: string, uri: string, version: string, message: string) {
-    
-  }
-}
---
+++ 03_Dependency-Injection-and-Services/movie-finder/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
